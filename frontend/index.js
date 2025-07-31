669528d7b5524f3fd9aff369e81edf46023ef202
Jaxson Brown <jokesene@outlook.com>
2025-07-31 15:26:44 +0800

    added html rendering

--- frontend/index.js
+++ frontend/index.js
@@ -20,7 +20,23 @@ async function getTodos() {
 
     // handle http response
     const todos = await response.json();
-    console.log(todos);
+
+    // render HTML
+    const todoItemsContainer = document.getElementById("todo-items");
+    todos.forEach((todo) => {
+        // create the list item element
+        const todoListItem = document.createElement("li");
+        todoListItem.innerHTML = todo.text;
+
+        // create buttons
+        const updateBtn = document.createElement("button");
+        const deleteBtn = document.createElement("button");
+
+        todoListItem.appendChild(updateBtn);
+        todoListItem.appendChild(deleteBtn);
+
+        todoItemsContainer.appendChild(todoListItem);
+    });
 }
 
 getTodos();
